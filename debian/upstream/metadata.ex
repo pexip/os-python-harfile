06c4657f1a6d5f8e00cd6f0960af8ff53d2658ca
Huw Jones <huw@pexip.com>
2025-05-09 13:04:32 +0000

    Prepare Pexip packaging.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,6 @@
+Bug-Database: https://github.com/schemathesis/harfile/issues
+Bug-Submit: https://github.com/schemathesis/harfile/issues/new
+Changelog: https://github.com/schemathesis/harfile/blob/main/CHANGELOG.md
+Documentation: https://github.com/schemathesis/harfile/wiki
+Repository-Browse: https://github.com/schemathesis/harfile
+Repository: https://github.com/schemathesis/harfile.git
